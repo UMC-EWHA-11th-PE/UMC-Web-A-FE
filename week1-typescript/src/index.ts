9d06686037a3aa48c606ccdf96205a762ea4103d
seohyeonS2 <shyeon16@ewha.ac.kr>
2026-09-19 20:29:03 +0900

    feat: 선택 미션 구현

--- week1-typescript/src/index.ts
+++ week1-typescript/src/index.ts
@@ -3,6 +3,7 @@ interface Member {
     name: string;
     role: string;
     githubId?: string;
+    studyHour?: number;
 }
 
 const members: Member[]=[
@@ -28,3 +29,38 @@ function memberMessage(id:number):string{
 console.log(memberMessage(1));
 console.log(memberMessage(2));
 console.log(memberMessage(999));
+
+// 선택 미션 1
+type StudyMember={
+    id: number;
+    name: string;
+    role: string;
+    githubId?: string;
+    studyHour?: number;
+}
+
+// 선택 미션 2
+const studyHour:number|undefined=0;
+const withOr=studyHour||1;
+const withNullish=studyHour??1;
+
+console.log(studyHour);
+console.log(withOr);
+console.log(withNullish);
+
+// 선택 미션 3
+function formatMemberId(input:unknown){
+    if(typeof input === "string"){
+        return memberMessage(Number(input));
+    }
+    if(typeof input === "number"){
+        return memberMessage(input);
+    }
+    else{
+        return null;
+    }
+}
+
+console.log(formatMemberId("1"));
+console.log(formatMemberId(2));
+console.log(formatMemberId(true));
