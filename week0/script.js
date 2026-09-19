53fbf1e7d37872f6ea655358132528954ab55439
mirikim404 <mirikim8989@gmail.com>
2026-09-19 16:37:06 +0900

    feat: 종합 실습 자기소개 카드 완성

--- week0/script.js
+++ week0/script.js
@@ -1,6 +1,6 @@
+const message = document.querySelector("#message");
+const cheerButton = document.querySelector("#cheer-button");
 
-const button = document.querySelector("#start-button");
-
-button.addEventListener("click", function () {
-    console.log("시작합니다!");
+cheerButton.addEventListener("click", function () {
+  message.textContent = "좋아요! 작은 코드부터 직접 바꾸어 봅시다. 🚀";
 });
